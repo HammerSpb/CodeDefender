62da70537f79631ef5f786dc0bf01ae1c84bbb23
Roman Fedoseev <Fedoseev.Roman@gmail.com>
2025-03-09 01:04:45 -0500

    add changes

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,4 +1,5 @@
 import { resolve } from 'path';
+import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -7,10 +8,22 @@ export default defineConfig({
     environment: 'node',
     include: ['test/**/*.e2e-spec.ts'],
     setupFiles: ['test/setup.ts'],
+    reporters: ['default', 'html'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
+    root: '.',
+    testTimeout: 20000, // Longer timeout for e2e tests
   },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
 });
